7e2d27823df14a496892cfa4aaae0225e270c309
10xdev4u-alt <10xdev4u@gmail.com>
2026-01-12 03:11:20 +0530

    feat(sdk): add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="princetheme-sdk",
+    version="1.0.0",
+    description="The Enterprise Grade SDK for GRUB Theming",
+    author="PrinceTheProgrammer",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'princetheme=sdk.cli:main',
+        ],
+    },
+)
